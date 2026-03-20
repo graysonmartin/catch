6747cf583e87f1bf1b773be611994a8e22c4299c
Grayson Martin <grayson.martin@enrian.com>
2026-03-20 09:36:17 +0100

    chore: remove stale TODO comment from AppURL

--- catch/Theme/AppURL.swift
+++ catch/Theme/AppURL.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 enum AppURL {
-    // TODO: Replace with hosted URLs before App Store submission
     static let privacyPolicy = URL(string: "https://graysonmartin.github.io/catch/privacy-policy.html")!
     static let termsOfService = URL(string: "https://graysonmartin.github.io/catch/terms-of-service.html")!
 }
